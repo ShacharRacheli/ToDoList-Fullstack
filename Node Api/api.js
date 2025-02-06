279f76dd85971f40b9bd6d14b942f4cd7e255489
shacharracheli <r0583237001@gmail.com>
2025-02-06 18:00:05 +0200

    render key

--- Node Api/api.js
+++ Node Api/api.js
@@ -21,31 +21,3 @@ app.get('/services', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-// app.get('/services', async (req, res) => {
-//     try {
-//       const apiKey = process.env.API_RENDER_KEY;
-//       if (!apiKey) {
-//         throw new Error('RENDER_API_KEY is missing');
-//       }
-  
-//       const response = await axios.get('https://api.render.com/v1/services', {
-//         headers: {
-//           Authorization: `Bearer ${apiKey}`,
-//         },
-//       });
-  
-//       res.json(response.data);
-//     } catch (error) {
-//       console.error('Error fetching data from Render API:', error.response?.status || error.message);
-//       res.status(error.response?.status || 500).json({
-//         message: 'Error fetching data',
-//         error: error.message,
-//         details: error.response?.data,
-//       });
-//     }
-//   });
-  
-//   const PORT = process.env.PORT || 3001;
-//   app.listen(PORT, () => {
-//     console.log(`✅ Server running at http://localhost:${PORT}`);
-//   });
